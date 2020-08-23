1da4aa959d84dc7a2379e2b614ce8c09ad61bf19
Ryan Altimari <ryanaltimari8@gmail.com>
2020-08-23 15:12:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -18,11 +18,11 @@ end
 
 #def reverse_each_word(string)
 # arr = string.split(" ")
-  reversed = ""
-  counter = 0
-  while counter < arr.length
-    return arr[counter].reverse
-    counter += 1
-  end
- reversed
-end
+#  reversed = ""
+#  counter = 0
+#  while counter < arr.length
+#    return arr[counter].reverse
+#    counter += 1
+#  end
+# reversed
+#end
